9162982d39e2be7c5ff7119c9855357e0e3c0472
tyloo <jbonva@gmail.com>
2017-12-20 16:43:26 +0100

    Added form requests
    Signed-off-by: tyloo <jbonva@gmail.com>

---
+++ app/Http/Requests/TagRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TagRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        if ('PUT' == $this->method()) {
+            return [
+                'name' => 'required|max:255',
+            ];
+        }
+
+        return [
+            'name' => 'required|max:255',
+            'slug' => 'required|max:255|unique:tags',
+        ];
+    }
+}
---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        if ('PUT' == $this->method()) {
+            return [
+                'name' => 'required|max:255',
+                'password' => 'confirmed|min:6',
+            ];
+        }
+
+        return [
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|confirmed|min:6',
+        ];
+    }
+}
